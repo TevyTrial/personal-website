4da842060268a8d6d6fb96ffbb878c8af779aca0
TevyTrial <clh49@sussex.ac.uk>
2026-03-09 14:21:12 +0000

    trying to redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename= "/personal-website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
