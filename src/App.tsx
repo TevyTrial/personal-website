983ad689aafba17feb2dffcc8b0fe7759cb9ffff
TevyTrial <clh49@sussex.ac.uk>
2026-03-13 13:41:06 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename=" ">
+      <BrowserRouter basename="/personal-website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
