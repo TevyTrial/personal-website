70d262193ea47c4c849bcd3fb014d375f4cf25bc
TevyTrial <clh49@sussex.ac.uk>
2026-03-13 17:10:00 +0000

    download cv edit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,7 +21,7 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo placeholder - left side */}
           <div className="flex-shrink-0">
-            <div className="w-8 h-8 rounded-lg overflow-hidden flex items-center justify-center">
+            <div className="w-10 h-10 rounded-lg overflow-hidden flex items-center justify-center">
               <img src={logoSrc} alt="Logo" className="w-10 h-10 object-contain rounded-lg" />
             </div>
           </div>
